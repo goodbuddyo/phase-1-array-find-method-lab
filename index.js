38cb549496ccd73fd467c8a5eb13a302c19db8f9
Seano <sean01234@yahoo.com>
2023-04-04 22:16:51 -0400

    solve sadly puzzle

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" }
+]
+
+function superbowlWin(arrayOfObjs) {
+  const sadly = arrayOfObjs.find(({ result }) => result === "W");
+  console.log(sadly);
+  if (sadly === undefined) {
+    return sadly
+  } else {
+    console.log(sadly.year)
+    return sadly.year
+  }
+}
+superbowlWin(record);
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
